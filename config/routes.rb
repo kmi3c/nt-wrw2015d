762c98f9fb0d50696b4a8e54616989ed8d48d3f6
padamski <pawel.adamski@connectmedica.com>
2016-09-23 10:23:51 +0200

    [WIP] Fix routes problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'visitors#index'
+  devise_for :users
   resources :students do
     get :subjects
   end
+  resources :teachers
+  get '/reports/subjects', to: 'reports#index', as: :report_subjects
 end
