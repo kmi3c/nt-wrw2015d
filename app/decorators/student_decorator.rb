2fe9e0f38b118b5ef9573d0d6331c987acf8a16f
padamski <pawel.adamski@connectmedica.com>
2016-09-23 14:22:59 +0200

    Student decorator average notes return proper values

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -1,8 +1,15 @@
 class StudentDecorator < BaseDecorator
+  include ActionView::Helpers::NumberHelper
   def full_name
     "#{first_name} #{last_name}"
   end
 
   def avg_notes(subject_item)
+    notes =  subject_item_notes.for_subject_item(subject_item)
+    if notes.count > 0
+      number_with_precision( notes.average(:value), precision: 2 )
+    else
+     '0.00'
+    end
   end
 end
