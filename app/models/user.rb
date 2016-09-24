402931bbc57d997b31b6125d1b3734d56ce31668
padamski <pawel.adamski@connectmedica.com>
2016-09-24 14:56:03 +0200

    Fix code typo and climate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :recoverable
-  validates_presence_of :password
-  validates_presence_of :email, if: :password?
-  validates_uniqueness_of :email, if: :password?
+  validates_presence_of :password, :email
+  validates_uniqueness_of :email
   validates_format_of :email,:with => Devise::email_regexp
 
   def password?
