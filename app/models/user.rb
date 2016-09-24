ee6cd698b8c5e5e4c045e2d063e34b12884dd8df
padamski <pawel.adamski@connectmedica.com>
2016-09-24 14:50:09 +0200

    User email uniqenes and password required
    Added email uniqenes due to devise default behaviour and test that
    required blank password validation. Email uniqueness and presence
    should be validated also beacouse of database schema.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :recoverable
+  validates_presence_of :password
+  validates_presence_of :email, if: :password?
+  validates_uniqueness_of :email, if: :password?
+  validates_format_of :email,:with => Devise::email_regexp
+
+  def password?
+    password.blank?
+  end
 end
